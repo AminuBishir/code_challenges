09297732ec3aa642e618ed22d59a0007b00fbaa7
Aminu Bishir <aminubishir@gmail.com>
2019-12-07 19:58:42 +0100

    adding description to the hour of glass problem solution

--- hour_glass.py
+++ hour_glass.py
@@ -1,4 +1,29 @@
 '''
+Title: Adding solution for hour glass challegen
+From: HackerRank -https://www.hackerrank.com/challenges/2d-array/problem
+
+Description:
+
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+We define an hourglass in  to be a subset of values with indices falling
+in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+
+There are  hourglasses in , and an hourglass sum is the sum of an
+hourglass' values. Calculate the hourglass sum for every hourglass in ,
+then print the maximum hourglass sum.
+
 INPUT: a 2D 6x6 array of integers
 OUTPUT: The sum of numbers in a hour glass having the highest sum
 
