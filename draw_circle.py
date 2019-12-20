83284e77954595842e980241fe78013ecc0e355b
Aminu Bishir <aminubishir@gmail.com>
2019-12-20 11:40:34 +0100

    Draw Circle Challenge
    This challenge make use of the turtle module to draw a circle by
    creating a series of squares that made a 360 degree circle.

---
+++ draw_circle.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_cicular_square():
+    
+    window = turtle.Screen()
+    window.bgcolor("green")
+    line = turtle.Turtle()
+    line.shape("turtle")
+    line.speed(50)
+    line.color("yellow")
+    for a in range(36):
+        line.right(10)
+        
+        for i in range(4):
+            line.forward(100)
+            line.right(90)
+
+    
+    window.exitonclick()
+
+    
+draw_cicular_square()
+
